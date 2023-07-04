4f70668eb2ce406c3e8e5f506a4f42939616dc27
Rohit Sharma <rohit.sharma3@assurant.com>
2023-07-04 15:57:35 +0530

    Adding proj changes

---
+++ src/ePrism.API.ClaimsDataSharing.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ePrism.API.ClaimsDataSharing.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
